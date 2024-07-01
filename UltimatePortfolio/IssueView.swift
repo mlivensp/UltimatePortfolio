05c83fd7df4ddf5071529e3afebdeb76aaf27e67
michaellivenspargar <mlivensp@gmail.com>
2024-07-01 15:57:46 -0700

    through episode 15

--- UltimatePortfolio/IssueView.swift
+++ UltimatePortfolio/IssueView.swift
@@ -59,22 +59,42 @@ struct IssueView: View {
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .animation(nil, value: issue.issueTagsList)
                 }
-                
             }
-        }
-        
-        Section {
-            VStack(alignment: .leading) {
-                Text("Basic information")
-                    .font(.title2)
-                    .foregroundStyle(.secondary)
-                
-                TextField("Description", text: $issue.issueContent, prompt: Text("Enter the issue description here"), axis: .vertical)
+            
+            Section {
+                VStack(alignment: .leading) {
+                    Text("Basic information")
+                        .font(.title2)
+                        .foregroundStyle(.secondary)
+                    
+                    TextField("Description", text: $issue.issueContent, prompt: Text("Enter the issue description here"), axis: .vertical)
+                }
             }
         }
         .disabled(issue.isDeleted)
         .onReceive(issue.objectWillChange) { _ in
             dataController.queueSave()
+        }
+        .onSubmit {
+            dataController.save()
+        }
+        .toolbar {
+            Menu {
+                Button {
+                    UIPasteboard.general.string = issue.title
+                } label: {
+                    Label("Copy Issue Title", systemImage: "doc.on.doc")
+                }
+                
+                Button {
+                    issue.completed.toggle()
+                    dataController.save()
+                } label: {
+                    Label(issue.completed ? "Re-open Issue" : "Close Issue", systemImage: "bubble.left.and.exclamationmark.bubble.right")
+                }
+            } label: {
+                Label("Actions", systemImage: "ellipsis.circle")
+            }
         }
     }
 }
